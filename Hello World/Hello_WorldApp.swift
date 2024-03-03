f54e0dfd82775c111de3254b6c83b5d0b8d10ec1
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-02 22:26:59 -0500

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Jason Fang on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
